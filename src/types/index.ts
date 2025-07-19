bb4e44a74a3ea677f90d59c292586fa7417a8750
Manu Jayakumar <99114381+manujayakumar@users.noreply.github.com>
2025-07-19 13:56:09 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,13 +2,3 @@ export interface Data{
     email: string | null;
     phoneNumber: string | null;
 }
-
-
-// enum LinkPrecedence{
-//     PRIMARY = "PRIMARY",
-//     SECONDARY = "SECONDARY"
-// }
-
-// export interface NewData extends Data {
-//   linkPrecedence: LinkPrecedence;
-// }
